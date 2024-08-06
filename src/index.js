d1c92c9b0bbde3cc5d9455d485a5cbb52530523a
Abdeali <abdealidahodwala163@gmail.com>
2024-08-06 23:47:51 +0530

    FEAT: added consuming queue event using rabbitmq and mail event completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const amqplib = require('amqplib');
 
-const { ServerConfig } = require("./config");
+const { ServerConfig, MAILER } = require("./config");
 const apiRoutes = require("./routes");
+const { EmailService } = require("./services");
+const serverConfig = require("./config/server-config");
+const {GMAIL_ID} = serverConfig
+
+async function connectQueue(){
+  try {
+      const connection = await amqplib.connect("amqp://localhost");
+      const channel = await connection.createChannel();
+      await channel.assertQueue("noti-queue");
+      channel.consume("noti-queue",async (data)=>{
+        // console.log(`${Buffer.from(data.content)}`);
+        const object = JSON.parse(`${Buffer.from(data.content)}`);
+        await EmailService.sendEmail(GMAIL_ID,object.recipientEmail,object.subject,object.text)
+        channel.ack(data);
+      })
+  } catch (error) {
+      console.log(error);
+      
+  }
+}
 
 const app = express();
 app.use(express.json());
@@ -10,4 +31,5 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  connectQueue()
 });
